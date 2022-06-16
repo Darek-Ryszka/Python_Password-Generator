f7ba9465ecac6928c0b1f45c0ff79df4e4f03321
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-16 09:15:10 +0200

    Add password list and length condition

--- Password_generator.py
+++ Password_generator.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+password = []
 
+password_length = int(input("How long the password should be?"))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if password_length < 5:
+    print("The password should be at least 5 characters long! Try again.")
+    sys.exit(0)
