e4083aca63a6d22e467ae9e5bccadb7951dbfb25
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-16 09:28:48 +0200

    Add lowercase letters length verification

--- Password_generator.py
+++ Password_generator.py
@@ -13,6 +13,10 @@ else:
 
 lowercase_letters = int(input("How many lowercase letters should the password have?"))
 
+if lowercase_letters > characters_left:
+    print("To many signs!")
+    sys.exit(0)
+
 uppercase_letters = int(input("How many uppercase letters should the password have?"))
 
 special_characters = int(input("How many special characters should the password have?"))
