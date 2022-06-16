39e3df8262c8ff3b20a26e4416da4ecb97a5a54c
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-16 09:51:51 +0200

    Add free characters left information

--- Password_generator.py
+++ Password_generator.py
@@ -8,7 +8,7 @@ def update_characters_left(number_of_characters):
     global characters_left
 
     if number_of_characters < 0 or number_of_characters > characters_left:
-        print("To many signs! You chose:", characters_left)
+        print("To many characters! You chose:", characters_left)
         sys.exit(0)
     else:
         characters_left -= number_of_characters
@@ -34,3 +34,6 @@ update_characters_left(special_characters)
 
 digits = int(input("How many digits should the password have? "))
 update_characters_left(digits)
+
+if characters_left > 0:
+    print("Not all characters were used! Password will be completed by lowercase letters!")
