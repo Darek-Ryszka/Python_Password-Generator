e24050cf088ff2f98c7411f6d0785c223c84d9b2
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-16 09:43:34 +0200

    Add update characters left function

--- Password_generator.py
+++ Password_generator.py
@@ -3,6 +3,18 @@ import sys
 password = []
 characters_left = -1
 
+
+def update_characters_left(number_of_characters):
+    global characters_left
+
+    if number_of_characters < 0 or number_of_characters > characters_left:
+        print("To many signs! You chose:", characters_left)
+        sys.exit(0)
+    else:
+        characters_left -= number_of_characters
+    print("Characters left: ", characters_left)
+
+
 password_length = int(input("How long the password should be? "))
 
 if password_length < 5:
@@ -12,14 +24,10 @@ else:
     characters_left = password_length
 
 lowercase_letters = int(input("How many lowercase letters should the password have? "))
-
-if lowercase_letters < 0 or lowercase_letters > characters_left:
-    print("To many signs! You chose:", characters_left)
-    sys.exit(0)
-else:
-    characters_left -= lowercase_letters
+update_characters_left(lowercase_letters)
 
 uppercase_letters = int(input("How many uppercase letters should the password have? "))
+update_characters_left(uppercase_letters)
 
 special_characters = int(input("How many special characters should the password have? "))
 
